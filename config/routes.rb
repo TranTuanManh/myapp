e5fb9f1f567d1753b4279c444ff1fc78135d1d8b
TranTuanManh <manhit1706@gmail.com>
2018-05-25 09:24:23 +0700

    Use SSL and the puma webserver

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/signup', to: 'users#new'
+  resources :users
 
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
 
   root 'static_pages#home'
-
 end
